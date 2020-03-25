fa219c6d0125f6836c4c37b5cb7cb3e3a506b354
vineetha1996 <59989572+vineetha1996@users.noreply.github.com>
2020-03-24 21:48:48 -0500

    added description

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, I am vineetha');
+  res.end('Hello, I am vineetha and this is my first node.js');
 });
 
 server.listen(port, hostname, () => {
